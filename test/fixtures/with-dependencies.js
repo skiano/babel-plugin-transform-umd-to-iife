a04daa5910dfc541646a93161406c507aa3466ff
Gregory Skiano <gregoryskiano@gmail.com>
2018-12-12 21:42:07 -0800

    updating all fixtures

--- test/fixtures/with-dependencies.js
+++ test/fixtures/with-dependencies.js
@@ -1,11 +1,13 @@
 (function (global, factory) {
   if (typeof define === "function" && define.amd) {
-    define(['exports', 'library', 'other-library'], factory);
+    define(['library', 'other-library'], factory);
   } else if (typeof exports !== "undefined") {
     // who cares....
   } else {
     factory(global.Library, global.OtherLibrary);
   }
 })(this, function () {
-  return {}
+  return {
+    isModule: true,
+  }
 });
